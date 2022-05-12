95b2041c668931eb6a4ac54f1d6e108934cd1b80
Alexey <a@cherepanov.info>
2022-05-12 10:52:38 +0300

    fix url

--- run.sh
+++ run.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-NAME=ip_api
+#NAME=ip_api
 
 #docker rm -f $NAME
 #docker build --no-cache -t $NAME .
 #docker run -it --name $NAME $NAME
 
 docker-compose build
+docker-compose stop
 docker-compose up
 
