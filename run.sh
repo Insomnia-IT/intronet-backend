1ba779dc86218f52f580f148f9c4d01a53708292
root <a@cherepanov.info>
2022-06-10 03:12:17 +0300

    fix run script

--- run.sh
+++ run.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-#NAME=ip_api
-
-#docker rm -f $NAME
-#docker build --no-cache -t $NAME .
-#docker run -it --name $NAME $NAME
-
 docker-compose build
 docker-compose stop
-docker-compose up
+docker-compose up -d
 
