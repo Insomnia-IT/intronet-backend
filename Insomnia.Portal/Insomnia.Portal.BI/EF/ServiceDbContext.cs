2b0c29c6774e8ebeec4d4db63623350ec6176efa
Ivan <jwplaster.com@gmail.com>
2022-07-15 11:53:25 +0300

    fix

--- Insomnia.Portal/Insomnia.Portal.BI/EF/ServiceDbContext.cs
+++ Insomnia.Portal/Insomnia.Portal.BI/EF/ServiceDbContext.cs
@@ -26,7 +26,7 @@ namespace Insomnia.Portal.EF
 
         public ServiceDbContext(DbContextOptions<ServiceDbContext> option) : base(option)
         {
-            //Database.EnsureDeleted();
+            Database.EnsureDeleted();
             Database.EnsureCreated();
         }
 
